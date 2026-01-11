68d052be7efa34bad128047c29bc3898a801650b
NGLK <107441000+NGLkoh@users.noreply.github.com>
2026-01-11 09:42:59 +0800

    fixed

--- app/src/main/java/com/aorv/blazerider/Notification.kt
+++ app/src/main/java/com/aorv/blazerider/Notification.kt
@@ -1,14 +1,21 @@
 package com.aorv.blazerider
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.PropertyName
 
 data class Notification(
-    @DocumentId val documentId: String = "",
+    @DocumentId 
+    val documentId: String = "",
+    
     val actorId: String? = null,
     val createdAt: Timestamp? = null,
     val entityId: String? = null,
     val entityType: String = "",
-    val isRead: Boolean = false,
+    
+    @get:PropertyName("isRead")
+    @set:PropertyName("isRead")
+    var isRead: Boolean = false,
+    
     val message: String = "",
     val metadata: Map<String, Any> = emptyMap(),
     val type: String = "",
